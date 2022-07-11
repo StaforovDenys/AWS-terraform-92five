f441bb0f4b38eb8e2598dd15836fb4b87fa66248
Denys Staforov <53435580+StaforovDenys@users.noreply.github.com>
2022-07-11 21:11:29 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+resource "aws_elastic_beanstalk_application" "elasticapp" {
+  name = var.elasticapp
+}
+
+# Create elastic beanstalk Environment
+
+resource "aws_elastic_beanstalk_environment" "beanstalkappenv" {
+  name                = var.beanstalkappenv
+  application         = aws_elastic_beanstalk_application.elasticapp.name
+  solution_stack_name = var.solution_stack_name
+  tier                = var.tier
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     =  "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "MatcherHTTPCode"
+    value     = "200"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.medium"
+  }
+}
