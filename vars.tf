5bf6d0f19ef73da45c1a662f4b4b2a8552388393
Denys Staforov <53435580+StaforovDenys@users.noreply.github.com>
2022-07-11 21:12:58 +0300

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "elasticapp" {
+  default = "92five-terra"
+}
+variable "beanstalkappenv" {
+  default = "92five-env"
+}
+variable "solution_stack_name" {
+  type = string
+}
+variable "tier" {
+  type = string
+}
